59831fb01ea3eb845e9f13f0d24cd92c90991d31
OFFOM <3333084444@qq.com>
2024-10-14 22:34:28 +0800

    test

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,9 +2,9 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = '小猪AI Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = '小猪AI';
+export const BRANDING_LOGO_URL = 'https://s3.mnat.cn/2024/10/14/670d288d81d1b.png';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '小猪Hub';
